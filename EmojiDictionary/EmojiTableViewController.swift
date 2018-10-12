3ebb15bb709f76bea100ae09dc4f74b00a74c7b4
Andreas Plagens <andreas@macbookpro.local>
2018-10-12 19:23:52 +0200

    1st complete running version

--- EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -69,8 +69,6 @@ class EmojiTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let emoji = emojis[indexPath.row]
-        print("\(emoji.symbol) \(indexPath)")
     }
     
 
@@ -129,7 +127,18 @@ class EmojiTableViewController: UITableViewController {
     
     @IBAction func unwindToEmojiTableView(segue:UIStoryboardSegue) {
         
+        guard segue.identifier == "saveUnwind" else { return }
+        let sourceViewController = segue.source as! AddEditEmojiTableViewController
+        
+        if let emoji = sourceViewController.emoji {
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                emojis[selectedIndexPath.row]  = emoji
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            } else {
+                let newIndexPath = IndexPath(row: emojis.count, section: 0)
+                emojis.append(emoji)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
+        }
     }
-    
-
 }
