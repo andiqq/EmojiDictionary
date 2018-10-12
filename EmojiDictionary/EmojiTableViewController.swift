8f111b91b41ad02a1168310f10cdb2b60cfe3d03
Andreas Plagens <andreas@macbookpro.local>
2018-10-12 15:53:51 +0200

    running version with 2nd screen for editing

--- EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -114,14 +114,22 @@ class EmojiTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "EditEmoji" {
+            let indexPath = tableView.indexPathForSelectedRow!
+            let emoji = emojis[indexPath.row]
+            let addEditEmojiTableViewController = segue.destination as! AddEditEmojiTableViewController
+            addEditEmojiTableViewController.emoji = emoji
+        }
     }
-    */
+    
+    @IBAction func unwindToEmojiTableView(segue:UIStoryboardSegue) {
+        
+    }
+    
 
 }
